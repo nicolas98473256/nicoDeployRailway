d5c0af91479ab6733b58e3df08966f924e5b8626
nicolas <nicog32f@gmail.com>
2024-11-11 13:33:04 -0500

    c1

--- Back_End/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Back_End/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,11 +1,11 @@
 package com.example.demo;
 
+import com.example.sport_full.DemoApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DemoApplication.class)
 class DemoApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
